a7b7e557381ecdc50b162b38cd2dac11e96c830c
icebud1818 <gws18115@gmail.com>
2026-01-22 18:37:49 -0500

    headless

--- calculators/sealedProduct.py
+++ calculators/sealedProduct.py
@@ -24,7 +24,7 @@ class CollectrScraper:
     def _init_driver(self):
         """Initialize or restart the Chrome driver"""
         chrome_options = Options()
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-blink-features=AutomationControlled')
